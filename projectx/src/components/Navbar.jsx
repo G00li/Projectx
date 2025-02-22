3317f6e625ae16ad0eab19c62c6cedca33d767d8
Leandro Oliveira <leandro.gooli@gmail.com>
2025-02-22 20:05:35 +0000

    Fix: Navbar Add icons

--- projectx/src/components/Navbar.jsx
+++ projectx/src/components/Navbar.jsx
@@ -1,25 +1,26 @@
-import Link from "next/link"
-import { NAV_LINKS } from "@/constants"
-import Image from "next/image"
-
-
+import Link from 'next/link';
+import { NAV_LINKS } from '@/constants';
+import Image from 'next/image';
 
 const Navbar = () => {
-  return(
+  return (
     <nav className="flex justify-between items-center max-container padding-container relative z-30 py-3 px-5 border-b border-gray-700">
       <Link href="/">
-        <Image src="logo/logo-gray.svg" alt="logo" width={40} height={40} />
+        <Image src="/logo/logo-gray.svg" alt="logo" width={40} height={40} />
       </Link>
 
-      <ul className="hidden h-full gap-12 lg:flex items-center">
+      <ul className="hidden h-full gap-12 lg:flex items-center mr-10">
         {NAV_LINKS.map((link) => (
-          <Link href={link.href} key={link.key} className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">
-            {link.label}
-          </Link>
+          <li key={link.key}>
+            <Link href={link.href} className="flex items-center gap-2 text-gray-50 cursor-pointer pb-1.5 transition-all hover:font-bold">
+              <Image src={link.icon} alt={`${link.label} icon`} width={20} height={20} />
+              <span className="hidden sm:block">{link.label}</span>
+            </Link>
+          </li>
         ))}
       </ul>
     </nav>
   );
-}
+};
 
 export default Navbar;
