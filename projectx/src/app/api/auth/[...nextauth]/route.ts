3856f9591bfe7618ea0a3cff6ed0fc6f8f6184c7
Leandro Oliveira <leandro.gooli@gmail.com>
2025-02-25 21:09:09 +0000

    Add: Nextauth Gmail

--- projectx/src/app/api/auth/[...nextauth]/route.ts
+++ projectx/src/app/api/auth/[...nextauth]/route.ts
@@ -1,26 +1,34 @@
 import { PrismaClient } from "@prisma/client";
-import NextAuth, { User } from "next-auth";
-import Github from "next-auth/providers/github";
-import Google from "next-auth/providers/google";
-import { JWT } from "next-auth/jwt";
+import NextAuth, { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 const prisma = new PrismaClient();
 
-const handler = NextAuth({
+export const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(prisma), // Usa o Prisma para gerenciar usuários no banco
     providers: [
-        Github({
+        GithubProvider({
             clientId: process.env.GITHUB_CLIENT_ID!,
             clientSecret: process.env.GITHUB_CLIENT_SECRET!,
         }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        }),
     ],
-
     callbacks: {
-        async jwt(
-            {token} : {token : JWT}
-        ) {
-            return token;
+        async session({ session, user }) {
+            if (session.user) {
+                session.user.id = user.id; // Adiciona o ID do usuário na sessão
+            }
+            return session;
         },
     },
-});
+    secret: process.env.NEXTAUTH_SECRET,
+};
+
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
