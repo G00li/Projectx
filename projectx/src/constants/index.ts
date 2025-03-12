df5f4a991983a89d4fbe6e536dd605558071943b
Leandro Oliveira <leandro.gooli@gmail.com>
2025-03-12 21:19:28 +0000

    Feat: Add icon Sidebar and New post

--- projectx/src/constants/index.ts
+++ projectx/src/constants/index.ts
@@ -1,7 +1,7 @@
 export const NAV_LINKS = [
   { href: '/', key: 'home', label: 'Home', icon: '/icon/home-icon.svg' },
-  { href: '/createPost', key: 'CreatePost', label: 'Create Posts', icon: '/icon/new-post.svg' },
-  { href: '/viewPost', key: 'ViewPost', label: 'View Posts', icon: '/icon/new-post.svg' },
+  { href: '/createPost', key: 'CreatePost', label: 'Create Posts', icon: '/icon/create-post.svg' },
+  { href: '/viewPost', key: 'ViewPost', label: 'View Posts', icon: '/icon/view-post.svg' },
   { href: '/search', key: 'Search', label: 'Search', icon: '/icon/search-icon.svg' },
   { href: '../pages/profile', key: 'Profile', label: 'Profile', icon: '/icon/profile-icon.svg' },
 ];
