a6ad4b611940a461adc574b96a636eddd27aa018
Leandro Oliveira <leandro.gooli@gmail.com>
2025-03-20 20:22:04 +0000

    Fix: Redirect to  ViewPost page

--- projectx/src/app/page.tsx
+++ projectx/src/app/page.tsx
@@ -1,8 +1,5 @@
-export default function HomePage() {
-  return (
-    <>
-      <h1>Olá Mundo</h1>
-    </>    
-  );
+import { redirect } from 'next/navigation';
 
+export default function HomePage() {
+  redirect('/viewAllPost');
 }
