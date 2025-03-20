d4ac1c4d692b56a65e3e4d11bd330381149f954a
Leandro Oliveira <leandro.gooli@gmail.com>
2025-03-20 20:00:51 +0000

    Feat: Add myPosts page

--- projectx/src/constants/index.ts
+++ projectx/src/constants/index.ts
@@ -2,7 +2,7 @@ export const NAV_LINKS = [
   { href: '/viewAllPost', key: 'ViewAllPosts', label: 'All Posts', icon: '/icon/sidebar/viewAllPosts-icon.svg' },
   { href: '/createPost', key: 'CreatePost', label: 'Create Posts', icon: '/icon/sidebar/create-post.svg' },
   { href: '/search', key: 'Search', label: 'Search', icon: '/icon/sidebar/search-icon.svg' },
-  { href: '/MyPosts', key: 'ViewPost', label: 'My Posts', icon: '/icon/sidebar/myPosts-icon.svg' },
+  { href: '/myPosts', key: 'ViewPost', label: 'My Posts', icon: '/icon/sidebar/myPosts-icon.svg' },
   { href: '/saved', key: 'Saved', label: 'Saved', icon: '/icon/sidebar/guardado-icon.svg' },
   { href: '/pages/profile', key: 'Profile', label: 'Profile', icon: '/icon/sidebar/profile-icon.svg' },
 ];
