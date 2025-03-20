d7de96f1e06f44ec818fae556ec759bcf2f59bc2
Leandro Oliveira <leandro.gooli@gmail.com>
2025-03-20 20:54:43 +0000

    Fix: Alterando tags

--- projectx/src/constants/index.ts
+++ projectx/src/constants/index.ts
@@ -1,8 +1,8 @@
 export const NAV_LINKS = [
-  { href: '/viewAllPost', key: 'ViewAllPosts', label: 'All Posts', icon: '/icon/sidebar/viewAllPosts-icon.svg' },
-  { href: '/createPost', key: 'CreatePost', label: 'Create Posts', icon: '/icon/sidebar/create-post.svg' },
-  { href: '/search', key: 'Search', label: 'Search', icon: '/icon/sidebar/search-icon.svg' },
-  { href: '/myPosts', key: 'ViewPost', label: 'My Posts', icon: '/icon/sidebar/myPosts-icon.svg' },
-  { href: '/saved', key: 'Saved', label: 'Saved', icon: '/icon/sidebar/guardado-icon.svg' },
-  { href: '/pages/profile', key: 'Profile', label: 'Profile', icon: '/icon/sidebar/profile-icon.svg' },
+  { href: '/viewAllPost', key: 'ViewAllPosts', label: 'Todos os Posts', icon: '/icon/sidebar/viewAllPosts-icon.svg' },
+  { href: '/createPost', key: 'CreatePost', label: 'Criar Post', icon: '/icon/sidebar/create-post.svg' },
+  { href: '/search', key: 'Search', label: 'Pesquisar Usuários', icon: '/icon/sidebar/search-icon.svg' },
+  { href: '/myPosts', key: 'ViewPost', label: 'Meus Posts', icon: '/icon/sidebar/myPosts-icon.svg' },
+  { href: '/saved', key: 'Saved', label: 'Posts Salvos', icon: '/icon/sidebar/guardado-icon.svg' },
+  { href: '/pages/profile', key: 'Profile', label: 'Perfil', icon: '/icon/sidebar/profile-icon.svg' },
 ];
